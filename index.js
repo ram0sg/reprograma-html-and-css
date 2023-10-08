02a3e85d096f12c3034728d2a4f2bf5024cc3c9a
Diana Martine <Diana_martine@outlook.com>
2023-10-08 11:48:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5173;
 
 app.use(express.static(path.join(__dirname, "src")));
 
